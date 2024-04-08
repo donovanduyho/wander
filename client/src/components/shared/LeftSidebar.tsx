5080d3b6ef81f4f07b0dbd16632558007e2e6d18
donovanduyho <donovanduyho@gmail.com>
2024-04-08 15:21:36 -0400

    implemented sidebar

--- client/src/components/shared/LeftSidebar.tsx
+++ client/src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,69 @@
 "use client";
 
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+
 function LeftSidebar() {
-    return <section></section>;
+    const router = useRouter();
+    const pathname = usePathname();
+
+    return (
+        <section className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto bg-neutral-900 border-r pb-5 pt-28 max-md:hidden">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => {
+                    const isActive =
+                        (pathname.includes(link.route) &&
+                            link.route.length > 1) ||
+                        pathname === link.route;
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`relative flex justify-start gap-4 rounded-lg p-4 ${
+                                isActive && "bg-blue-500"
+                            }`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+
+                            <p className="text-white font-semibold max-lg:hidden">
+                                {link.label}
+                            </p>
+                        </Link>
+                    );
+                })}
+            </div>
+
+            <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton
+                        signOutCallback={() => router.push("/sign-in")}
+                    >
+                        <div className="flex cursor-pointer gap-4 p-4">
+                            <Image
+                                src="/assets/logout.svg"
+                                alt="logout"
+                                width={17}
+                                height={17}
+                            />
+
+                            <p className="text-white font-semibold max-lg:hidden">
+                                Logout
+                            </p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+            </div>
+        </section>
+    );
 }
 
 export default LeftSidebar;
---
+++ client/src/constants/index.js
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        imgURL: "/assets/home.svg",
+        route: "/",
+        label: "Home",
+    },
+    {
+        imgURL: "/assets/discover.svg",
+        route: "/discover",
+        label: "Discover",
+    },
+    {
+        imgURL: "/assets/community.svg",
+        route: "/community",
+        label: "Community",
+    },
+    {
+        imgURL: "/assets/profile.svg",
+        route: "/profile",
+        label: "Profile",
+    },
+];
