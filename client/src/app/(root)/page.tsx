97462c7ca9a1031fa93b3eae34cd43a7b397c254
donovanduyho <donovanduyho@gmail.com>
2024-04-10 15:08:07 -0400

    tweaks

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    useEffect(() => {
+        fetch("http://localhost:8000/test/")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }, []);
+
     return (
         <>
             <h1 className="text-left">Home</h1>
--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const path = require('path');
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const path = require("path");
 const router = express.Router();
 
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
+app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(passport.initialize());
-require('./controllers/jwt')(passport);
+require("./controllers/jwt")(passport);
 
-app.use("/", require('./routes/routes'));
+app.use("/", require("./routes/routes"));
 
 const port = 8000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
