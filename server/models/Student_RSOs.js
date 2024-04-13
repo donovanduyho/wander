0b79235912da11d9ff2ea7500524bce8590438f8
ajm1312 <andrew.mitchell11@yahoo.com>
2024-04-13 06:44:32 -0400

    test

--- server/models/Student_RSOs.js
+++ server/models/Student_RSOs.js
@@ -11,12 +11,6 @@ async function findRSOMemberPidRid(pid, rid) {
 
 async function addRSOMember(member) {
     const {rid, pid} = member
-    database.query("UPDATE Person SET rid = ? WHERE pid = ?", [rid, pid])
-    .then(() => console.log("person updates"))
-    .catch((err) => {
-        console.log(err);
-        throw err;
-    })
     return database.query("INSERT INTO Student_RSOs (rid, pid) VALUES (?, ?)", [rid, pid])
     .then(([id]) => id.insertId)
     .catch((err) => {
@@ -27,11 +21,6 @@ async function addRSOMember(member) {
 
 async function deleteRSOMember(member) {
     const {pid, rid} = member;
-    database.query("UPDATE Person SET rid = NULL WHERE pid = ?", [pid])
-    .catch(err => {
-        console.log(err);
-        throw err;
-    })
     return database.query("DELETE FROM Student_RSOs WHERE pid = ? AND rid = ?", [pid, rid])
     .catch((err) => {
         console.log(err);
