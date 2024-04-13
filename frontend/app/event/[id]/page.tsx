9245076540ba39b365958ecebfc10cddfb2a26e1
donovanduyho <donovanduyho@gmail.com>
2024-04-13 06:34:18 -0400

    completed rso join/leave feature

--- frontend/app/event/[id]/page.tsx
+++ frontend/app/event/[id]/page.tsx
@@ -14,8 +14,16 @@ interface Event {
     description: string;
 }
 
+interface Comment {
+    cid: string;
+    event_comment: string;
+    rating: string;
+    post_time: string;
+}
+
 const Page = ({ params }: { params: { id: string } }) => {
     const [event, setEvent] = useState<Event | null>(null);
+    const [comments, setComments] = useState<Comment[]>([]);
 
     useEffect(() => {
         const getEvent = async () => {
@@ -24,30 +32,44 @@ const Page = ({ params }: { params: { id: string } }) => {
                     `http://localhost:8000/events/${params.id}`
                 );
                 setEvent(response.data);
-                console.log(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        const getComments = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:8000/comment/${params.id}/list`
+                );
+                setComments(response.data);
+                console.log(response.data.event_comment);
             } catch (error) {
                 console.log(error);
             }
         };
 
         getEvent();
+        getComments();
     }, [params.id]);
 
     return (
         <section>
             {event ? (
-                <EventCard
-                    eid={event.eid}
-                    name={event.name}
-                    event_location={event.event_location}
-                    time={event.time}
-                    category={event.category}
-                    description={event.description}
-                />
+                <div>
+                    <EventCard
+                        eid={event.eid}
+                        name={event.name}
+                        event_location={event.event_location}
+                        time={event.time}
+                        category={event.category}
+                        description={event.description}
+                    />
+                    <h2>Comments</h2>
+                </div>
             ) : (
                 <p>Loading...</p>
             )}
-
             <Comment />
         </section>
     );
