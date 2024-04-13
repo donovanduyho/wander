f8a1720e72e79b7bf4aa5dcc21887b4bda5525f4
ajm1312 <andrew.mitchell11@yahoo.com>
2024-04-13 03:31:26 -0400

    fixed comment

--- server/routes/comments_route.js
+++ server/routes/comments_route.js
@@ -10,13 +10,13 @@ const {
 } = require('../models/Comment')
 
 router.get('/:eid/comments', (req, res) => {
-    const {eid} = req.param
+    const {eid} = req.params
     listComments(eid).then((data) => res.status(200).json({data}))
     .catch((err) => res.status(400).json({ message: "Something went wrong fetching comments"}))
 })
 
-router.post('/:eid/post'), (req, res) => {
-    const {eid} = req.param;
+router.post('/:eid/post', (req, res) => {
+    const {eid} = req.params;
     const {pid} = req.body;
     const {
         event_comment,
@@ -33,10 +33,16 @@ router.post('/:eid/post'), (req, res) => {
     addComment(newComment).then(() => res.status(200).json({message: "Successfully added comment"}))
     .catch(() => res.status(400).json({ message: "Error adding comment"}));
 
-}
+})
+
+router.get("/:eid/list", (req, res) => {
+    const eid = req.params
+    listComments(eid).then((data) => res.status(200).json({ data }))
+    .catch(() => res.status(400).json({ message: "Error listing comments"}))
+})
 
 router.delete('/:eid/delete', (req, res) => {
-    const {eid} = req.param;
+    const {eid} = req.params;
     const {pid} = req.user;
     const comment = {
         eid,
